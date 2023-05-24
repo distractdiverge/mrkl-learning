e60a7176fcadcb8f4d5155f5279954362f2ebf76
Alex Lapinski <alex.lapinski@gmail.com>
2023-05-24 17:31:23 -0400

    Added logging templates/scafolding

---
+++ services/logging.py
@@ -0,0 +1,25 @@
+from enum import Enum
+from abc import ABC, abstractmethod
+
+class LogLevel(Enum):    
+    DEBUG = 3
+    INFO = 2
+    WARN = 1
+    ERROR = 0
+
+class AbstractLogger(ABC):
+    @abstractmethod()
+    def log(self, level:LogLevel, message:str) -> None:
+        pass
+
+    def debug(self, message:str) -> None:
+        return self.log(LogLevel.DEBUG, message)
+
+    def info(self, message:str) -> None:
+        return self.log(LogLevel.INFO, message)
+
+    def warn(self, message:str) -> None:
+        return self.log(LogLevel.WARN, message)
+
+    def error(self, message:str) -> None:
+        return self.log(LogLevel.ERROR, message)
