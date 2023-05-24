73678a4184d4a178377eb479fe91c013c1e3a5d7
Alex Lapinski <alex.lapinski@gmail.com>
2023-05-24 10:34:12 -0400

    Adding more tools & personas to babyagi

--- baby-agi.py
+++ baby-agi.py
@@ -13,6 +13,8 @@ from langchain.experimental import BabyAGI
 from langchain.vectorstores import FAISS
 from langchain.docstore import InMemoryDocstore
 
+from langchain.tools import ShellTool
+
 # Define your embedding model
 embeddings_model = OpenAIEmbeddings()
 # Initialize the vectorstore as empty
@@ -29,12 +31,25 @@ todo_prompt = PromptTemplate.from_template(
     "You are a planner who is an expert at coming up with a todo list for a given objective. Come up with a todo list for this objective: {objective}"
 )
 todo_chain = LLMChain(llm=OpenAI(temperature=0), prompt=todo_prompt)
+
+analyze_prompt = PromptTemplate.from_template(
+    "You are a multi-disciplinary scientist. You have extensive background in human biology, and chemistry. Review the following information and produce an analysis of the claim.: {information}"
+)
+analyze_chain = LLMChain(llm=OpenAI(temperature=0), prompt=analyze_prompt)
+
 search = SerpAPIWrapper()
+
 tools = [
+    ShellTool(),
     Tool(
         name="Search",
         func=search.run,
         description="useful for when you need to answer questions about current events",
+    ),
+    Tool(
+        name="Analyze",
+        func=search.run,
+        description="useful for when you need to analyze a large amount of information",
     ),
     Tool(
         name="TODO",
@@ -63,11 +78,11 @@ agent_executor = AgentExecutor.from_agent_and_tools(
 )
 
 
-OBJECTIVE = "Write a weather report for Philadelphia today"
+OBJECTIVE = "Search scholarly articles to determine what form of blueberries are the most effective at helping short term memory."
 # Logging of LLMChains
 verbose = False
 # If None, will keep on going forever
-max_iterations: Optional[int] = 3
+max_iterations: Optional[int] = 20
 baby_agi = BabyAGI.from_llm(
     llm=llm, vectorstore=vectorstore, task_execution_chain=agent_executor, verbose=verbose, max_iterations=max_iterations
 )
